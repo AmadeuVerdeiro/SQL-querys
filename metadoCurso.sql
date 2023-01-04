2c27aa8b38d6b2510ab25ff7ab1e543c76bb86df
Amadeu Verdeiro <117334604+AmadeuVerdeiro@users.noreply.github.com>
2023-01-04 11:20:04 +0100

    Update metadoCurso.sql

--- metadoCurso.sql
+++ metadoCurso.sql
@@ -12,3 +12,16 @@ join customer as cus using(customer_id)
 group by customer_id
   having total >=150 and compras >= 35
     order by total desc
+--------------------------------------------------------------------------------
+
+use sakila;
+create or replace view vendas_por_cliente as
+select 
+	cus.customer_id,
+    cus.first_name,
+    cus.last_name,
+    pay.amount
+from customer cus
+join payment pay
+	on cus.customer_id = pay.payment_id
+    order by pay.amount asc
